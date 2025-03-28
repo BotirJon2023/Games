2fcd3bd171a974b4920ce6ec0cef0db9d270372d
Botirjon Ruzmetov <132999244+BotirJon2023@users.noreply.github.com>
2025-03-28 08:23:59 +0100

    Add files via upload
    Dark web horror exploration gane with Java code example

---
+++ DarkWebHorrorExploration/DarkWebHorrorExploration/src/DarkWebHorrorExploration.java
@@ -0,0 +1,215 @@
+
+import javax.sound.sampled.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class DarkWebHorrorExploration extends JPanel implements ActionListener, KeyListener {
+    private Timer timer;
+    private ArrayList<DarkEntity> entities;
+    private Player player;
+    private Random random;
+    private boolean flickering;
+    private int flickerCounter;
+    private int playerHealth;
+    private boolean gameOver;
+
+    public DarkWebHorrorExploration() {
+        timer = new Timer(100, this);
+        entities = new ArrayList<>();
+        player = new Player(300, 250);
+        random = new Random();
+        flickering = false;
+        flickerCounter = 0;
+        playerHealth = 3;
+        gameOver = false;
+
+        for (int i = 0; i < 5; i++) {
+            entities.add(new DarkEntity(random.nextInt(600), random.nextInt(500)));
+        }
+
+        timer.start();
+        setFocusable(true);
+        addKeyListener(this);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        if (flickering && flickerCounter % 2 == 0) {
+            g2d.setColor(Color.BLACK);
+            g2d.fillRect(0, 0, getWidth(), getHeight());
+            return;
+        }
+
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+
+        for (DarkEntity entity : entities) {
+            entity.draw(g2d);
+        }
+
+        player.draw(g2d);
+
+        g2d.setColor(Color.RED);
+        g2d.drawString("Health: " + playerHealth, 10, 20);
+
+        if (gameOver) {
+            g2d.setFont(new Font("Arial", Font.BOLD, 50));
+            g2d.setColor(Color.RED);
+            g2d.drawString("GAME OVER", 200, 250);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (!gameOver) {
+            for (DarkEntity entity : entities) {
+                entity.update();
+                if (entity.touches(player)) {
+                    playerHealth--;
+                    playSound("scream.wav");
+                    if (playerHealth <= 0) {
+                        gameOver = true;
+                        timer.stop();
+                    }
+                }
+            }
+        }
+
+        if (random.nextInt(100) > 95) {
+            flickering = true;
+            flickerCounter = 10;
+        }
+
+        if (flickering) {
+            flickerCounter--;
+            if (flickerCounter <= 0) {
+                flickering = false;
+            }
+        }
+
+        repaint();
+    }
+
+    private void playSound(String soundFile) {
+        try {
+            File file = new File(soundFile);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (!gameOver) {
+            int key = e.getKeyCode();
+            if (key == KeyEvent.VK_LEFT) {
+                player.move(-10, 0);
+            } else if (key == KeyEvent.VK_RIGHT) {
+                player.move(10, 0);
+            } else if (key == KeyEvent.VK_UP) {
+                player.move(0, -10);
+            } else if (key == KeyEvent.VK_DOWN) {
+                player.move(0, 10);
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Dark Web Horror Exploration");
+        DarkWebHorrorExploration game = new DarkWebHorrorExploration();
+
+        frame.setSize(600, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(game);
+        frame.setVisible(true);
+    }
+}
+
+class DarkEntity {
+    private int x, y;
+    private float transparency;
+    private boolean fadingIn;
+    private Random random;
+
+    public DarkEntity(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.transparency = 0.1f;
+        this.fadingIn = true;
+        this.random = new Random();
+    }
+
+    public void update() {
+        if (fadingIn) {
+            transparency += 0.02f;
+            if (transparency >= 1.0f) {
+                fadingIn = false;
+            }
+        } else {
+            transparency -= 0.02f;
+            if (transparency <= 0.1f) {
+                x = random.nextInt(600);
+                y = random.nextInt(500);
+                transparency = 0.1f;
+                fadingIn = true;
+            }
+        }
+    }
+
+    public boolean touches(Player player) {
+        return Math.abs(player.getX() - x) < 40 && Math.abs(player.getY() - y) < 50;
+    }
+
+    public void draw(Graphics2D g2d) {
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, transparency));
+        g2d.setColor(Color.WHITE);
+        g2d.fillOval(x, y, 80, 100);
+    }
+}
+
+class Player {
+    private int x, y;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void draw(Graphics2D g2d) {
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+        g2d.setColor(Color.RED);
+        g2d.fillRect(x, y, 30, 30);
+    }
+}
+
+
